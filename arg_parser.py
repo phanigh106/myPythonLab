10607b93978b04942ceae86c323ae45f2d70a067
Naga Phaneendra Ghantasala <nghantas@qti.qualcomm.com>
2025-02-26 11:18:22 +0530

    arg parser

--- arg_parser.py
+++ arg_parser.py
@@ -3,11 +3,16 @@ import sys
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='demo argument parser',description="test script to demo argparse",
-                                     epilog="test epilog")
-    parser.add_argument("-m","--message",help= "message for print function")
+    #parent parser imported to import args from parent parser
+    parent_parser = argparse.ArgumentParser(prog="parent_parser", description="desc for parent parser",add_help=False)
+    parent_parser.add_argument("-c", type=int)
+
+    parser = argparse.ArgumentParser(prog='demo_argument_parser',description="test script to  \n $$$$$$$$$$$ \n demo argparse",formatter_class=argparse.MetavarTypeHelpFormatter,
+                                     epilog="test epilog",usage="demo_argument_parser [options]",parents=[parent_parser])
+    parser.add_argument("message",type=str,default="dssfdsfdsf",help= "message for print function(positional argument)")
     parser.add_argument("-a",type=int,help="first value")
     parser.add_argument("-b", type=int,help="second value")
+
     if len(sys.argv) == 1:
         parser.print_help()
         exit(1)
@@ -16,6 +21,8 @@ def main():
         print_message(message=args.message)
     if args.a and args.b:
         print_summation(a=args.a,b=args.b)
+    if args.c:
+        print(args.c)
 
 
 def print_message(message=None):
